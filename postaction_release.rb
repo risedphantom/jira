0e6a8dd0909ee010ddf25d388cb170745dda21c1
Sutiagin Alexey <alesut@users.noreply.github.com>
2016-06-01 12:02:53 +0300

    SRV-1282 auto merge to master (#43)
    * SRV-1282: Add sublime to gitignore
    
    * SRV-1282: Close pullrequests after release

--- postaction_release.rb
+++ postaction_release.rb
@@ -24,6 +24,29 @@ STDOUT.sync = true
 options = { auth_type: :basic }.merge(opts.to_hash)
 client = JIRA::Client.new(options)
 release = client.Issue.find(opts[:release])
+raise "WTF??? release search returned #{release.length} elements!" if (release.is_a? Array) && (release.length > 1)
+release = release[0] if release.is_a? Array
+
+pullrequests = release.pullrequests(SimpleConfig.git.to_h)
+                      .filter_by_status('OPEN')
+                      .filter_by_source_url(SimpleConfig.jira.issue)
+
+unless pullrequests.valid?
+  release.post_comment p("ReviewRelease: #{pullrequests.valid_msg}")
+  exit
+end
+
+pullrequests.each do |pr|
+  # Checkout repo
+  puts "Clone/Open with #{pr.dst} branch #{pr.dst.branch} and merge #{pr.src.branch} and push".green
+  begin
+    pr.repo.push
+  rescue Git::GitExecuteError => e
+    puts e.message.red
+    next
+  end
+end
+
 release.deploys.each do |issue|
   puts issue.key
   # Transition to DONE
