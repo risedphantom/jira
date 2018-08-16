cb8123357bd7d4731d94fa850ddc1d5e2d6327cd
Sergey <hroft356@mail.ru>
2018-08-16 09:10:56 +0300

    QA-1093 rollback notify (#168)
    Аdd notify events in task

--- lib/scenarios/rollbackrelease.rb
+++ lib/scenarios/rollbackrelease.rb
@@ -4,18 +4,40 @@ module Scenarios
   class RollbackRelease
     def run
       LOGGER.info "Starting RollbackRelease for #{SimpleConfig.jira.issue}"
-      jira = JIRA::Client.new SimpleConfig.jira.to_h
+      jira    = JIRA::Client.new SimpleConfig.jira.to_h
       release = jira.Issue.find(SimpleConfig.jira.issue)
-      release.rollback
-      release.linked_issues('deployes').each do |issue|
-        trans = 'Not merged'
-        if issue.has_transition?(trans)
-          LOGGER.info "Rollback issue '#{issue.key}': transition to '#{trans}'"
-          issue.transition trans
-        else
-          LOGGER.warn "Rollback issue '#{issue.key}': transition '#{trans}' not found"
+      release.post_comment <<-BODY
+      {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
+        Запущен откат релиза (!)
+        Ожидайте сообщение о завершении
+      {panel}
+      BODY
+      begin
+        release.rollback
+        release.linked_issues('deployes').each do |issue|
+          trans = 'Not merged'
+          if issue.has_transition?(trans)
+            LOGGER.info "Rollback issue '#{issue.key}': transition to '#{trans}'"
+            issue.transition trans
+          else
+            LOGGER.warn "Rollback issue '#{issue.key}': transition '#{trans}' not found"
+          end
         end
+      rescue StandardError
+        release.post_comment <<-BODY
+        {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
+         Не удалось откатить релиз (x)
+         Подробности в логе таски https://jenkins.twiket.com/view/RELEASE/job/rollback_release/
+        {panel}
+        BODY
+        exit(1)
       end
+      # Write message in release ticket
+      release.post_comment <<-BODY
+        {panel:title=Release notify!|borderStyle=dashed|borderColor=#ccc|titleBGColor=#E5A443|bgColor=#F1F3F1}
+          Откат релиза завершен (/)
+        {panel}
+      BODY
     end
   end
 end
