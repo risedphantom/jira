273e18ee1b4355477cb927ce9cc272200e3033b1
sergey <sergey.matveev@onetwotrip.com>
2018-10-26 19:30:23 +0300

    /

--- lib/common/bitbucket.rb
+++ lib/common/bitbucket.rb
@@ -4,6 +4,8 @@ require 'tinybucket'
 Tinybucket.configure do |config|
   config.oauth_token = ENV['BITBUCKET_OAUTH_TOKEN']
   config.oauth_secret = ENV['BITBUCKET_OAUTH_SECRET']
+  LOGGER.info "Token: #{config.oauth_token}"
+  LOGGER.info "Token: #{config.oauth_secret}"
 end
 
 BITBUCKET = Tinybucket.new
