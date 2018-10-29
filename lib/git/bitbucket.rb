00b23dfcfeea2e8564a200144d264ff681c44e51
sergey <sergey.matveev@onetwotrip.com>
2018-10-29 15:38:05 +0300

    refactor create_release task

--- lib/git/bitbucket.rb
+++ lib/git/bitbucket.rb
@@ -9,20 +9,7 @@ module Git
   class Base
     # Create pull request from src branch to dst
     # By default: from local branch to master
-    def create_pullrequest(src: current_branch, dst: 'master', repo: remote.url.repo, username: nil, password: nil, oauth: nil)
-      request = { title: "#{src} #{repo}",
-                  source: { branch: { name: src },
-                            repository: { full_name: repo } },
-                  destination: { branch: { name: dst } } }
-      begin
-        url = "https://#{username}:#{password}@api.bitbucket.org/2.0/repositories/#{repo}/pullrequests"
-        RestClient.post url, request.to_json, content_type: :json
-      rescue StandardError => e
-        puts "Error: #{e}; URL: #{url}; PARAMS: #{request}"
-      end
-      end
-
-    def create_pullrequest_new(username, password, src)
+    def create_pullrequest(username = nil, password = nil, src = current_branch)
       request = { title: "#{src} #{remote.url.repo}",
                   source: { branch: { name: src },
                             repository: { full_name: remote.url.repo } },
