e3182ff2eee25b96cd6148fe39f018e52542f11d
Sergey <hroft356@mail.ru>
2019-06-19 11:31:38 +0300

    Slack notify (#200)
    * add release_properties file

--- lib/scenarios/createrelease.rb
+++ lib/scenarios/createrelease.rb
@@ -100,6 +100,7 @@ module Scenarios
       LOGGER.info "Storing '#{release.key}' to file, to refresh buildname in Jenkins"
       Ott::Helpers.export_to_file(release.key, 'release_name.txt')
       Ott::Helpers.export_to_file("export ISSUE=#{release.key}", 'set_env.sh')
+      Ott::Helpers.export_to_file("ISSUE=#{release.key}", 'release_properties')
     end
     # :nocov:
   end
